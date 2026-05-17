b372d77b7928a65ef59fdb1616858cad07d565d2
Lars (LazarusDeveloper) <lars@digitaltundra.com>
2026-05-17 18:23:51 +0000

    Revert "lcl: move Win32 darkstyle handler into interface package"
    This reverts commit f74aab0584034f5cff6d15bab4d0a019c14db5c9.

--- lcl/interfaces/win32/uimport.pas
+++
@@ -1,7 +0,0 @@
-{
-  Win32 interface package shim.
-  The implementation remains in lcl/darkstyle so LCLBase can share it without
-  adding the whole darkstyle source directory to the LCL interface search path.
-}
-
-{$I ../../darkstyle/uimport.pas}
